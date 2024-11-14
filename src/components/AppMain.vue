d87f74f91f641f4de9e5022fb417dbd7491d99f9
GiorgioDordo <djole6789@gmail.com>
2024-11-14 20:24:46 +0100

    milestone 3

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,11 +9,38 @@ export default {
 
     }
   },
+  methods: {
+    getProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects')
+      .then(function(response) {
+        console.log(response.data.results);
+      })
+      .catch(function(error){
+        console.log(error);
+      }) 
+      this.postList = response.data.results;
+    }
+  },
+  created() {
+    this.getProjects();
+  }
+  
 }
 </script>
 
 <template>
- <h1>Progetto</h1>
+<main class="container">
+    <div class="row d-flex justify-content-center">
+        <div><h1 class="text-center">PROJECTS</h1></div>
+        <div class="col-3 m-3 p-3 card">
+            <div class="card-body">
+                <p><strong></strong></p>
+                <p></p>
+                <p></p>
+            </div>
+        </div>
+    </div>
+</main>
 </template>
 
 <style scoped>
