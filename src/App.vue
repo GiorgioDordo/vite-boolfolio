859f79f945f0320de27e68cdba4c258456060b77
GiorgioDordo <djole6789@gmail.com>
2024-11-14 19:59:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  data() {
+    return{
+    }
+  },
+  components: {
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use '../node_modules/bootstrap/scss/bootstrap.scss' as *;
+@use './styles/general.scss' as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+//bisogna importare axios per poter fare la chiamata ajax
+// AJAX = Asyncronous JavaScript And Xml
+import axios from 'axios';
+
+export default {
+  data() {
+    return{
+
+    }
+  },
+}
+</script>
+
+<template>
+ <h1>Progetto</h1>
+</template>
+
+<style scoped>
+</style>
